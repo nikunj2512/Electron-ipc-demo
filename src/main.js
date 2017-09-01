168309ca76b18203d67b9cbfeef195a64a2340be
nikunj <nikunj2512@gmail.com>
2017-09-01 13:56:21 -0500

    Added System Tray with icon and context menu.

--- src/main.js
+++ src/main.js
@@ -1,33 +1,62 @@
 const electron = require('electron');
 const countdown = require('./countdown');
 const app = electron.app;
+const path = require('path');
 const BrowserWindow = electron.BrowserWindow;
 const ipc = electron.ipcMain;
+const Menu = electron.Menu;
+/* Electron System Tray*/
+const Tray = electron.Tray;
 
 const windows = [];
 
 app.on('ready', _ => {
-    [1,2,3].forEach(_ => {
-    let win = new BrowserWindow({
-        height: 400,
-        width: 400
-    });
+    [1].forEach(_ => {
+        let win = new BrowserWindow({
+            height: 400,
+            width: 400
+        });
 
-    win.loadURL(`file://${__dirname}/countdown.html`);
+        win.loadURL(`file://${__dirname}/countdown.html`);
 
-    win.on('closed', _ => {
-        win = null;
-    });
+        win.on('closed', _ => {
+            win = null;
+        });
 
-    windows.push(win);
+
+        const name = electron.app.getName();
+        const template = [{
+            label: name,
+            submenu: [{
+                label: `About ${name}`,
+                click: _ => {
+                    console.log('clicked about');
+                }
+            }, {
+                type: 'separator'
+            }, {
+                label: 'Quit',
+                click: _ => {
+                    app.quit()
+                },
+                accelerator: 'Ctrl+Q'
+            }]
+        }];
+
+        const tray = new Tray(path.join('src', 'Pikachu.png'));
+        const menu = Menu.buildFromTemplate(template);
+        Menu.setApplicationMenu(menu);
+        tray.setContextMenu(menu);
+        tray.setToolTip('Electron IPC');
+        windows.push(win);
     });
 });
 
-ipc.on('countdown-start', _ =>{
+ipc.on('countdown-start', _ => {
     countdown(count => {
         console.log('count', count);
-        windows.forEach(win =>{
-        win.webContents.send('countdown', count);
+        windows.forEach(win => {
+            win.webContents.send('countdown', count);
         });
     });
 });
